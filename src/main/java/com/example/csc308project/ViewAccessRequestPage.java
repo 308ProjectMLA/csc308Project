146bc1be2fa9c4b5a1f013f648b2cb8a997d175f
plandsman11 <parker.rey@gmail.com>
2021-11-03 16:27:31 +0100

    Created new permission pages

---
+++ src/main/java/com/example/csc308project/ViewAccessRequestPage.java
@@ -0,0 +1,42 @@
+package com.example.csc308project;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class ViewAccessRequestPage {
+
+    public VBox pageLayout(Stage primaryStage) {
+        VBox pageVBox = new VBox(20);
+        HBox titleBox = new HBox(10);
+
+        //create page title
+        titleBox.setAlignment(Pos.CENTER);
+        Text title = new Text("Access Request Page\t\t");
+
+        //back button
+        ManagePermissionPage managePermissionPage = new ManagePermissionPage();
+        Button backButton = new Button("Back to Permissions Page");
+        backButton.setOnAction(actionEvent -> {
+            primaryStage.setScene(new Scene(managePermissionPage.pageLayout(primaryStage), 500, 300));
+        });
+
+        titleBox.getChildren().addAll(title, backButton);
+
+        //create page content
+        Text exampleText =  new Text("\tNo current access requests for this file.");
+
+        //create page
+        pageVBox.getChildren().addAll(titleBox, exampleText);
+
+        return pageVBox;
+    }
+
+}
