93a9ee9df924916bb2f6dd3f27776050882e661d
Leander <lbalmeo@calpoly.edu>
2021-11-09 01:49:54 +0100

    updated main to make sure log in looks fine

--- src/main/java/com/example/csc308project/Main.java
+++ src/main/java/com/example/csc308project/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application {
         VBox loginBox = lip.logInPageLayout();
 
         Button submit = new Button("Submit");
-        loginBox.getChildren().add(5, submit);
+        loginBox.getChildren().add(3, submit);
         submit.setOnAction(actionEvent -> {
             if(isValid(lip.getPossCombos(), lip.getUsername().getCharacters().toString(), lip.getPassword().getCharacters().toString())){
                 AccountPage ap = new AccountPage();
