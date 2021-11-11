ce6466f7b32eaafc7b2be0f081dc005a63b847e1
Laura <lcamac02@calpoly.edu>
2021-11-11 13:57:08 -0800

    new branch update

--- src/main/java/com/example/csc308project/DummyFilePage.java
+++ src/main/java/com/example/csc308project/DummyFilePage.java
@@ -1,4 +1,16 @@
 package com.example.csc308project;
 
+import javafx.geometry.Pos;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
 public class DummyFilePage {
+    public VBox DummyFileLayout() {
+        VBox mainVBox = new VBox();
+        mainVBox.setAlignment(Pos.CENTER);
+        Text dummyText = new Text("dummy file");
+
+        mainVBox.getChildren().addAll(dummyText);
+        return mainVBox;
+    }
 }
