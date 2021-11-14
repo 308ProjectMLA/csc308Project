b6adf86823524e57a984e9531d511e71e7e2047f
Laura <lcamac02@calpoly.edu>
2021-11-14 12:13:57 -0800

    added file creation logic

--- src/main/java/com/example/csc308project/CreateFilePage.java
+++ src/main/java/com/example/csc308project/CreateFilePage.java
@@ -1,16 +1,56 @@
 package com.example.csc308project;
 
 import javafx.geometry.Pos;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
+import java.io.File;
+import java.io.IOException;
+
 public class CreateFilePage {
     Button backButton;
+    Button createButton;
+    private TextField fileName;
+    boolean result;
+
     public VBox CreateFileLayout() {
         Main.updateTitle("Create New File");
         VBox mainVBox = new VBox();
         mainVBox.setAlignment(Pos.CENTER);
+
+        //get name for new file
+        fileName  = new TextField();
+        fileName.setPromptText("Enter new file name");
+        fileName.setMaxWidth(200);
+
+        //create button
+        createButton = new Button("create");
+        createButton.setOnAction(actionEvent -> {
+            //checks to see that there is actually text in the file name
+            if(fileName.getCharacters().toString() != "" || fileName.getCharacters().toString() != "\n"){
+                System.out.println(fileName.getCharacters().toString());
+                //actually makes the file
+                //were only making text files rn lmao
+                File newFile = new File("data/"+ fileName.getCharacters().toString() +".txt");
+                try {
+                    result = newFile.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                System.out.println("empty");
+            }
+        });
+        //success message?
+        if(result){
+            //success bb
+        }else{
+            //failed
+        }
+        //open or back option
+
         backButton = new Button("back");
 
         FileSelectPage fsp = new FileSelectPage();
@@ -18,7 +58,7 @@ public class CreateFilePage {
             Main.updatePage(fsp.fileSelectLayout());
         });
 
-        mainVBox.getChildren().addAll(backButton);
+        mainVBox.getChildren().addAll(fileName, createButton, backButton);
         return mainVBox;
     }
 }
