366dc5c3531ff4a54c8bacf9a93a978e6ffe210b
Bryce Cordill <brycecordill@gmail.com>
2021-11-24 18:08:18 +0100

    Comments and constants

--- src/main/java/com/example/csc308project/ManifestParser.java
+++ src/main/java/com/example/csc308project/ManifestParser.java
@@ -17,7 +17,7 @@ public class ManifestParser {
 
     public static final String GROUP_TAG = "group";
     public static final String PERM_TAG = "perm";
-
+    public static final String USER_TAG = "user";
 
     public ManifestParser(String filename) {
         fname = "data/" + filename + ".mnf";
@@ -26,18 +26,23 @@ public class ManifestParser {
     public void createDefaultManifest() {
         JSONObject jo = new JSONObject();
 
+        // Array of users that have permissions
         JSONArray userArray = new JSONArray();
-
+        // Pairs of users and their permissions
         Map<String, String> userPerm = new LinkedHashMap<>(2);
 
-        userPerm.put("username", Main.currentUser.getUsername());
+        // Give the user creating the file read and write perms by default
+        userPerm.put(USER_TAG, Main.currentUser.getUsername());
         userPerm.put(PERM_TAG, "rw");
 
         userArray.add(userPerm);
 
+        // Add it to the user array
         jo.put("users", userArray);
 
+        // Array of groups that have permissions
         JSONArray groupArray = new JSONArray();
+        // Pairs of groups and their permissions
         Map<String, String> groupPerm = new LinkedHashMap<>(2);
 
         // TODO Want no group perms on file creation
@@ -48,6 +53,7 @@ public class ManifestParser {
 
         jo.put("groups", groupArray);
 
+        // Attempt to open the file
         PrintWriter pw = null;
         try {
             pw = new PrintWriter(fname);
@@ -56,6 +62,7 @@ public class ManifestParser {
         }
 
         assert pw != null;
+        // Write out the JSON
         pw.write(jo.toJSONString());
 
         pw.flush();
@@ -67,7 +74,7 @@ public class ManifestParser {
 
         JSONObject jo = (JSONObject) obj;
 
-        JSONArray users = (JSONArray) jo.get("user");
+        JSONArray users = (JSONArray) jo.get("users");
         JSONArray groups = (JSONArray) jo.get("groups");
 
         for (Object curr : groups) {
@@ -77,41 +84,50 @@ public class ManifestParser {
         }
     }
 
-    public boolean addPermission(String type, String value, char permission) throws IOException, ParseException {
+    // Type: "user" or "group", name: user or group name, char: 'r' or 'w'
+    public boolean addPermission(String type, String name, char permission) throws IOException, ParseException {
+        // Only two types: user and group
+        assert type.equals(USER_TAG) || type.equals(GROUP_TAG);
 
-        assert type.equals("user") || type.equals(GROUP_TAG);
-
+        // Read in the JSON file
         Object obj = new JSONParser().parse(new FileReader(fname));
         JSONObject jo = (JSONObject) obj;
         Map<String, String> newPerm = new LinkedHashMap<>(2);
 
+        // Get either the users or groups array
         JSONArray arr = (JSONArray) jo.get(type + "s");
+        // The object to remove (for updating) if a listing already exists
         Object toRemove = null;
 
         for (Object curr : arr) {
             JSONObject internal = (JSONObject) curr;
             // See if the group/user already has permissions
-            if (internal.get(type).equals(value)) {
+            if (internal.get(type).equals(name)) {
                 String permList = (String) internal.get(PERM_TAG);
-                // If not in the permission list
+                // If not in the permission list, add the permission
                 if (permList.indexOf(permission) == -1) {
                     permList += permission;
-                    newPerm.put(value, permList);
-                    newPerm.put(type, value);
+                    // Put the updated line in the mapping
+                    newPerm.put(name, permList);
+                    newPerm.put(type, name);
                     newPerm.put(PERM_TAG, permList);
+                    // Set the existing entry for removal
                     toRemove = internal;
                     break;
                 }
                 else {
+                    // If no update, return false
                     return false;
                 }
             }
         }
+        // Remove the old entry if it was updated
         if (toRemove != null) {
             arr.remove(toRemove);
         }
+        // Create a new entry if none were found
         else {
-            newPerm.put(type, value);
+            newPerm.put(type, name);
             newPerm.put(PERM_TAG, Character.toString(permission));
         }
 
