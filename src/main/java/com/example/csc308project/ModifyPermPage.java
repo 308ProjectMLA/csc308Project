5c79f2e2da5a377ede048ca4329df1bb8ba11951
plandsman11 <parker.rey@gmail.com>
2021-11-26 13:26:03 +0100

    Updated modify perm to selectors

--- src/main/java/com/example/csc308project/ModifyPermPage.java
+++ src/main/java/com/example/csc308project/ModifyPermPage.java
@@ -51,56 +51,117 @@ public class ModifyPermPage {
 
         header.getChildren().addAll(pageTitle, backButton);
 
-        //create page content
+        //File selector
         Text fileTitle = new Text("File to Manage");
         ComboBox<String> fileSelector = new ComboBox<>();
         fileSelector.setPromptText("Select file to manage");
 
         ArrayList<File> fileList = FileSelectController.getPermFiles();
-        // Sort the files by name
         fileList.sort((file1, file2) -> {
             assert !file1.getName().isBlank() && !file2.getName().isBlank();
-
             return file1.getName().compareTo(file2.getName());
         });
-
         for (File file : fileList) {
             String fileName = file.getName();
             fileSelector.getItems().add(fileName.substring(0, fileName.length() - 4));
         }
 
-        Text addGroupTitle = new Text("Group to add");
-        TextField groupAdd  = new TextField();
-        groupAdd.setPromptText("Enter group to add");
-        groupAdd.setMaxWidth(Main.FIELD_WIDTH);
+        //GROUPS
+        //group selector add write
+        ComboBox<String> groupAddWriteSelector = new ComboBox<>();
+        groupAddWriteSelector.setPromptText("Select group to add write");
+        groupAddWriteSelector.setMinWidth(215);
+        //group selector add read
+        ComboBox<String> groupAddReadSelector = new ComboBox<>();
+        groupAddReadSelector.setPromptText("Select group to add read");
+        groupAddReadSelector.setMinWidth(215);
+        //group selector remove write
+        ComboBox<String> groupRemoveWriteSelector = new ComboBox<>();
+        groupRemoveWriteSelector.setPromptText("Select group to remove write");
+        groupRemoveWriteSelector.setMinWidth(215);
+        //group selector remove read
+        ComboBox<String> groupRemoveReadSelector = new ComboBox<>();
+        groupRemoveReadSelector.setPromptText("Select group to remove read");
+        groupRemoveReadSelector.setMinWidth(215);
 
-        Text delGroupTitle = new Text("Group to remove");
-        TextField groupDel  = new TextField();
-        groupDel.setPromptText("Enter group to remove");
-        groupDel.setMaxWidth(Main.FIELD_WIDTH);
+        //add group names to selectors for groups
+        ArrayList<String> groupList = Group.parseGroup();
+        groupList.sort((group1, group2) -> {
+            assert !group1.isBlank() && !group2.isBlank();
+            return group1.compareTo(group2);
+        });
+        for (String group : groupList) {
+            groupAddWriteSelector.getItems().add(group);
+            groupAddReadSelector.getItems().add(group);
+            groupRemoveWriteSelector.getItems().add(group);
+            groupRemoveReadSelector.getItems().add(group);
+        }
 
-        Text addUserTitle = new Text("User to add");
-        TextField userAdd  = new TextField();
-        userAdd.setPromptText("Enter user to add");
-        userAdd.setMaxWidth(Main.FIELD_WIDTH);
+        //USERS
+        //user selector add write
+        ComboBox<String> userAddWriteSelector = new ComboBox<>();
+        userAddWriteSelector.setPromptText("Select user to add write");
+        userAddWriteSelector.setMinWidth(215);
+        //user selector add read
+        ComboBox<String> userAddReadSelector = new ComboBox<>();
+        userAddReadSelector.setPromptText("Select user to add read");
+        userAddReadSelector.setMinWidth(215);
+        //user selector remove write
+        ComboBox<String> userRemoveWriteSelector = new ComboBox<>();
+        userRemoveWriteSelector.setPromptText("Select user to remove write");
+        userRemoveWriteSelector.setMinWidth(215);
+        //user selector remove read
+        ComboBox<String> userRemoveReadSelector = new ComboBox<>();
+        userRemoveReadSelector.setPromptText("Select user to remove read");
+        userRemoveReadSelector.setMinWidth(215);
 
-        Text removeUserTitle = new Text("User to remove");
-        TextField userDel  = new TextField();
-        userDel.setPromptText("Enter user to remove");
-        userDel.setMaxWidth(Main.FIELD_WIDTH);
+        //add group names to selectors for groups
+        ArrayList<String> userList = User.getAllUsers();
+        userList.sort((user1, user2) -> {
+            assert !user1.isBlank() && !user2.isBlank();
+            return user1.compareTo(user2);
+        });
+        for (String user : userList) {
+            userAddWriteSelector.getItems().add(user);
+            userAddReadSelector.getItems().add(user);
+            userRemoveWriteSelector.getItems().add(user);
+            userRemoveReadSelector.getItems().add(user);
+        }
+
+        //Titles
+        Text addGroupReadTitle = new Text("Group to add read");
+        Text addGroupWriteTitle = new Text("Group to add write");
+        Text delGroupReadTitle = new Text("Group to remove read");
+        Text delGroupWriteTitle = new Text("Group to remove write");
+        Text addUserReadTitle = new Text("User to add read");
+        Text addUserWriteTitle = new Text("User to add write");
+        Text delUserReadTitle = new Text("User to remove read");
+        Text delUserWriteTitle = new Text("User to remove write");
+
+        VBox leftCol = new VBox(10);
+        leftCol.getChildren().addAll(addGroupReadTitle, groupAddReadSelector, delGroupReadTitle, groupRemoveReadSelector,
+        addUserReadTitle, userAddReadSelector, delUserReadTitle, userRemoveReadSelector);
+
+        VBox rightCol = new VBox(10);
+        rightCol.getChildren().addAll(addGroupWriteTitle, groupAddWriteSelector, delGroupWriteTitle, groupRemoveWriteSelector,
+                addUserWriteTitle, userAddWriteSelector, delUserWriteTitle, userRemoveWriteSelector);
+
+        HBox combineCols = new HBox(50);
+        combineCols.getChildren().addAll(leftCol, rightCol);
+        combineCols.setAlignment(Pos.CENTER);
+
 
         Button saveButton = new Button("Submit");
         saveButton.setOnAction(actionEvent -> {
-            String fileName = fileSelector.getValue();
-            String gdName = groupDel.getCharacters().toString();
-            String gaName = groupAdd.getCharacters().toString();
-            String udName = userDel.getCharacters().toString();
-            String uaName = userAdd.getCharacters().toString();
-            try {
-                processPermChange(fileName, gdName, gaName, udName, uaName);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+//            String fileName = fileSelector.getValue();
+//            String gdName = groupDel.getCharacters().toString();
+//            String udName = userDel.getCharacters().toString();
+//            String uaName = userAdd.getCharacters().toString();
+//            try {
+//                processPermChange(fileName, gdName, gaName, udName, uaName);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
 
         });
 
@@ -113,8 +174,7 @@ public class ModifyPermPage {
         box1.getChildren().addAll(saveButton, cancelButton);
         box1.setAlignment(Pos.CENTER);
 
-        buttonVBox.getChildren().addAll(message, fileTitle, fileSelector, addGroupTitle, groupAdd,
-                delGroupTitle, groupDel, addUserTitle, userAdd, removeUserTitle, userDel, box1);
+        buttonVBox.getChildren().addAll(message, fileTitle, fileSelector, combineCols,  box1);
         buttonVBox.setAlignment(Pos.CENTER);
 
         //create page
@@ -125,31 +185,31 @@ public class ModifyPermPage {
         return pageVBox;
     }
 
-    // returns if the user is valid and part of the system
-    private static boolean validUser(String user){
-        if(user == null || user.isBlank()){
-            return true;
-        }
-        ArrayList<String> userList = User.getAllUsers();
-        if(!userList.contains(user)){
-            message.setText(message.getText() + "Error: " + user + " is not a valid user");
-            return false;
-        }
-        return true;
-    }
-
-    // returns if the group is valid and part of the system
-    private static boolean validGroup(String group){
-        if(group == null || group.isBlank()){
-            return true;
-        }
-        ArrayList<String> groupList = Group.parseGroup();
-        if(!groupList.contains(group)){
-            message.setText(message.getText() + "Error: " + group + " is not a valid group");
-            return false;
-        }
-        return true;
-    }
+//    // returns if the user is valid and part of the system
+//    private static boolean validUser(String user){
+//        if(user == null || user.isBlank()){
+//            return true;
+//        }
+//        ArrayList<String> userList = User.getAllUsers();
+//        if(!userList.contains(user)){
+//            message.setText(message.getText() + "Error: " + user + " is not a valid user");
+//            return false;
+//        }
+//        return true;
+//    }
+//
+//    // returns if the group is valid and part of the system
+//    private static boolean validGroup(String group){
+//        if(group == null || group.isBlank()){
+//            return true;
+//        }
+//        ArrayList<String> groupList = Group.parseGroup();
+//        if(!groupList.contains(group)){
+//            message.setText(message.getText() + "Error: " + group + " is not a valid group");
+//            return false;
+//        }
+//        return true;
+//    }
 
     private static void processPermChange(String fileName, String gDelName, String gAddName, String uDelName, String uAddName) throws IOException, ParseException {
         message.setText("Permission Modification Submitted\n\n");
@@ -162,10 +222,9 @@ public class ModifyPermPage {
             message.setText(message.getText() + "Error: Please enter permission info to update\n");
             return ;
         }
-        if (!validGroup(gAddName) || !validGroup(gDelName) || !validUser(uAddName) || !validUser(uDelName)){
-            return;
-        }
-        System.out.println("MADE IT PAST VALIDATIONS");
+//        if (!validGroup(gAddName) || !validGroup(gDelName) || !validUser(uAddName) || !validUser(uDelName)){
+//            return;
+//        }
 
         ManifestParser manifestParser = new ManifestParser(fileName);
         if(gAddName != null && !gAddName.isBlank()){
